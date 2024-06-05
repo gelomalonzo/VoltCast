5937eedb765d296429f0c63227b8a2db630d61fa
gelomalonzo <abmalonzo@up.edu.ph>
2024-06-06 00:16:53 +0800

    Cleaned

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import io
 import pandas as pd
 import numpy as np
 import joblib
-from statsmodels.tsa.seasonal import seasonal_decompose
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from keras.models import load_model
 from tcn import TCN
 import plotly.graph_objects as go
@@ -31,75 +29,13 @@ if "show_welcome" not in st.session_state:
 if "show_results" not in st.session_state:
     st.session_state.show_results = False
 
-def deseason(ts_df:pd.DataFrame, season_df:pd.DataFrame) -> pd.DataFrame:
-    deseasoned_df = ts_df.copy()
-    for index, row in ts_df.iterrows():
-        for col in ts_df.columns:
-            deseasoned_df.loc[index, col] = deseasoned_df.loc[index, col] - season_df.loc[index.strftime("%m-%d"), col]
-    return deseasoned_df
-
-def revert_deseason(ts_df:pd.DataFrame, season_df:pd.DataFrame) -> pd.DataFrame:
-    deseasoned_df = ts_df.copy()
-    for index, row in ts_df.iterrows():
-        for col in ts_df.columns:
-            deseasoned_df.loc[index, col] = deseasoned_df.loc[index, col] + season_df.loc[index.strftime("%m-%d"), col]
-    return deseasoned_df
-
-def revert_detrend(df:pd.DataFrame, trend_scaler:MinMaxScaler, trend_df:pd.DataFrame, trend_model:object=None) -> pd.DataFrame:
-    detrended_df = df.copy()
-
-    X = get_input_sequences(trend_df)
-    preds = trend_model.predict(X)
-    preds = compress_to_timestep(preds)[-180:]
-    h_dates = [trend_df.index[-1] + timedelta(days=i) for i in range(1, 180 + 1)]
-    temp_trend_df = pd.DataFrame(preds, columns=df.columns, index=h_dates)
-
-    temp_trend_df[df.columns] = trend_scaler.inverse_transform(temp_trend_df[df.columns])
-    
-    # st.subheader("Trend Predictions")
-    # for col in numeric_cols:
-    #     fig = go.Figure()
-    #     fig.add_trace(go.Scatter(
-    #         x=trend_df.index,
-    #         y=trend_df[col],
-    #         mode="lines",
-    #         name="Past Data"
-    #     ))
-    #     fig.add_trace(go.Scatter(
-    #         x=temp_trend_df.index,
-    #         y=temp_trend_df[col],
-    #         mode="lines",
-    #         name="Forecasted"
-    #     ))
-    #     # if actual_data_file:
-    #     #     fig.add_trace(go.Scatter(
-    #     #         x=actual_data_df.index,
-    #     #         y=actual_data_df[col],
-    #     #         mode="lines",
-    #     #         name="Actual"
-    #     #     ))
-    #     fig.update_layout(
-    #         title=f"Trend Forecasts for {col}",
-    #         xaxis_title="Date",
-    #         yaxis_title="Value",
-    #         template="plotly_white"
-    #     )
-    #     st.plotly_chart(fig, use_container_width=True)
-    
-    for index, row in df.iterrows():
-        for col in df.columns:
-            detrended_df.loc[index, col] = detrended_df.loc[index, col] + temp_trend_df.loc[index, col]
-            
-    # st.write("---")
-    return detrended_df
-
-def get_input_sequences(df:pd.DataFrame) -> np.ndarray:
+def get_input_sequences(df:pd.DataFrame):
     X = []
     for i in range(len(df) - 180 + 1):
         X.append(df.iloc[i : i + 180].values)
     return np.array(X, dtype=np.float64)
 
-def compress_to_timestep(seq_arr:np.ndarray) -> np.ndarray:
+def compress_to_timestep(seq_arr:np.ndarray):
     temp_arr = []
     # Extract the first prediction from each sequence
     if len(seq_arr) > 0:
@@ -141,40 +77,57 @@ if st.session_state.show_welcome:
     with about_col:
         st.subheader("About")
         st.write("\
-            **VoltCast** is a simple web application designed for time series electricity forecasting.\
-            It integrates the recurrent neural networks GRU and LSTM \
-            to forecast electricity demand, price, and supply for up to 180 days ahead.\
-            Its goal is to incorporate deep learning in forecasting electricity data in the Philippines \
-            to provide insights that could help in the electricity operations \
+            **VoltCast** is a simple web application designed for time series \
+            electricity forecasting. It integrates the recurrent neural \
+            networks GRU and LSTM to forecast electricity demand, price, and \
+            supply for up to 180 days ahead. Its goal is to incorporate deep \
+            learning in forecasting electricity data in the Philippines to \
+            provide insights that could help in the electricity operations \
             (e.g., management, maintenance, costs) through its forecasts.\
         ")
         st.write("\
-            This web application was developed by Angelo Malonzo (abmalonzo@up.edu.ph) \
-            as an output of a comparative study on deep learning algorithms for electricity forecasting.\
+            This web application was developed by Angelo Malonzo \
+            (abmalonzo@up.edu.ph) as an output of a comparative study on \
+            deep learning algorithms for electricity forecasting.\
         ")
     with instructions_col:
         st.subheader("Instructions")
         st.write("\
-            The sidebar on the left contains the input form where users can upload the input data. \
+            The sidebar on the left contains the input form where \
+            users can upload the input data. \
         ")
         st.write("\
-            The first file upload bin is allotted for uploading the CSV file of the past data \
-            to be used as input to the model for it to draw its forecasts.\
+            The first file upload bin is allotted for uploading the CSV file of \
+            the past data to be used as input to the model for it to draw its forecasts.\
         ")
         st.write("\
-            The second file upload bin is for uploading the CSV file of the actual data if available. \
-            This is mainly for comparing the forecasts with the actual values to check the performance.\
+            The second file upload bin is for uploading the CSV file of the actual \
+            data if available. This is mainly for comparing the forecasts with the \
+            actual values to check the performance.\
         ")
         st.write("\
-            Once the input files are uploaded, click the **Forecast** button below the file upload bins to generate forecasts. \
-            The Results page will then be displayed showing the table and plots of the generated forecasts. \
+            Once the input files are uploaded, click the **Forecast** button below \
+            the file upload bins to generate forecasts. The Results page will then \
+            be displayed showing the table and plots of the generated forecasts. \
             Users can download the forecast table as a CSV file and the plots as PNG files.\
         ")
         st.subheader("Notes")
-        st.write("1. The input file for the historical data is required.")
-        st.write("2. The historical data must have at least 180 days of (consecutive) electricity data.")
-        st.write("3. The input data (historical and actual) must be in daily time steps.")
-        st.write("4. The input data (historical and actual) must have the following columns: DATE, BLOCK, LUZVIS PRICE, LUZON DEMAND, VISAYAS DEMAND, LUZON SUPPLY, VISAYAS SUPPLY.")
+        st.write("\
+            1. The input file for the historical data is required.\
+        ")
+        st.write("\
+            2. The historical data must have at least 180 days of \
+            (consecutive) electricity data.\
+        ")
+        st.write("\
+            3. The input data (historical and actual) must be in \
+            daily time steps."\
+        )
+        st.write("\
+            4. The input data (historical and actual) must have the following \
+            columns: DATE, BLOCK, LUZVIS PRICE, LUZON DEMAND, VISAYAS DEMAND, \
+            LUZON SUPPLY, VISAYAS SUPPLY.\
+        ")
         st.write("5. Ensure that there are no missing and absurd input data values.")
 
 # RESULTS PAGE
@@ -187,6 +140,11 @@ if st.session_state.show_results:
     past_data_text = past_data_file.read()
     past_data_text_str = str(past_data_text, "utf-8")
     past_data_df = pd.read_csv(io.StringIO(past_data_text_str))
+    past_data_df.columns = past_data_df.columns\
+                           .str.upper()\
+                           .str.strip()\
+                           .str.replace(r'\s+', ' ', regex=True)
+    past_data_df.replace(',', '', regex=True, inplace=True)
     past_data_df["DATE"] = pd.to_datetime(past_data_df["DATE"])
     past_data_df["DATE"] = past_data_df["DATE"].dt.date
     past_data_df.set_index("DATE", inplace=True)
@@ -194,16 +152,30 @@ if st.session_state.show_results:
         actual_data_text = actual_data_file.read()
         actual_data_text_str = str(actual_data_text, "utf-8")
         actual_data_df = pd.read_csv(io.StringIO(actual_data_text_str))
+        actual_data_df.columns = actual_data_df.columns\
+                                 .str.upper()\
+                                 .str.strip()\
+                                 .str.replace(r'\s+', ' ', regex=True)
+        actual_data_df.replace(',', '', regex=True, inplace=True)
         actual_data_df["DATE"] = pd.to_datetime(actual_data_df["DATE"])
         actual_data_df["DATE"] = actual_data_df["DATE"].dt.date
         actual_data_df.set_index("DATE", inplace=True)
     
     # import models
-    numeric_cols = ["LUZVIS PRICE (PHP/KWH)", "LUZON DEMAND (MW)", "VISAYAS DEMAND (MW)", "LUZON SUPPLY (MW)", "VISAYAS SUPPLY (MW)"]
+    numeric_cols = [
+        "LUZVIS PRICE (PHP/KWH)", 
+        "LUZON DEMAND (MW)", 
+        "VISAYAS DEMAND (MW)", 
+        "LUZON SUPPLY (MW)", 
+        "VISAYAS SUPPLY (MW)"
+    ]
     scaler = joblib.load("assets/scaler2.pkl")
-    init_gru_model = load_model("assets/init_gru.h5") # for "LUZON DEMAND (MW)", "VISAYAS DEMAND (MW)", "LUZON SUPPLY (MW)"
-    tuned_gru_model = load_model("assets/tuned_gru.h5") # for "LUZVIS PRICE (PHP/KWH)"
-    init_lstm_model = load_model("assets/init_lstm.h5") # for "VISAYAS SUPPLY (MW)"
+    # for "LUZON DEMAND (MW)", "VISAYAS DEMAND (MW)", "LUZON SUPPLY (MW)"
+    init_gru_model = load_model("assets/init_gru.h5") 
+    # for "LUZVIS PRICE (PHP/KWH)"
+    tuned_gru_model = load_model("assets/tuned_gru.h5") 
+    # for "VISAYAS SUPPLY (MW)"
+    init_lstm_model = load_model("assets/init_lstm.h5") 
     
     # preprocess
     actual_past_data_df = past_data_df.copy()
@@ -216,19 +188,31 @@ if st.session_state.show_results:
     
     init_gru_preds = init_gru_model.predict(X)
     init_gru_preds = compress_to_timestep(init_gru_preds)[-180:]
-    init_gru_preds_df = pd.DataFrame(init_gru_preds, index=pd.to_datetime(forecast_dates), columns=numeric_cols)
+    init_gru_preds_df = pd.DataFrame(
+        init_gru_preds, 
+        index=pd.to_datetime(forecast_dates), 
+        columns=numeric_cols
+    )
     init_gru_preds_df.index = init_gru_preds_df.index.date
     init_gru_preds_df[numeric_cols] = scaler.inverse_transform(init_gru_preds_df[numeric_cols])
     
     tuned_gru_preds = tuned_gru_model.predict(X)
     tuned_gru_preds = compress_to_timestep(tuned_gru_preds)[-180:]
-    tuned_gru_preds_df = pd.DataFrame(tuned_gru_preds, index=pd.to_datetime(forecast_dates), columns=numeric_cols)
+    tuned_gru_preds_df = pd.DataFrame(
+        tuned_gru_preds, 
+        index=pd.to_datetime(forecast_dates), 
+        columns=numeric_cols
+    )
     tuned_gru_preds_df.index = tuned_gru_preds_df.index.date
     tuned_gru_preds_df[numeric_cols] = scaler.inverse_transform(tuned_gru_preds_df[numeric_cols])
     
     init_lstm_preds = init_lstm_model.predict(X)
     init_lstm_preds = compress_to_timestep(init_lstm_preds)[-180:]
-    init_lstm_preds_df = pd.DataFrame(init_lstm_preds, index=pd.to_datetime(forecast_dates), columns=numeric_cols)
+    init_lstm_preds_df = pd.DataFrame(
+        init_lstm_preds, 
+        index=pd.to_datetime(forecast_dates), 
+        columns=numeric_cols
+    )
     init_lstm_preds_df.index = init_lstm_preds_df.index.date
     init_lstm_preds_df[numeric_cols] = scaler.inverse_transform(init_lstm_preds_df[numeric_cols])
     
@@ -274,3 +258,4 @@ if st.session_state.show_results:
             template="plotly_white"
         )
         st.plotly_chart(fig, use_container_width=True)
+        
